6167eb1edf73a24db0421939628ba328ce2c3b97
Shahid Hasan Shuvo <mrshanshuvo@gmail.com>
2025-06-16 16:15:39 +0600

    complete the module

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
@@ -7,8 +9,41 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 app.use(express.json());
+app.use(cookieParser());
+
+// Logger middleware to log request method and URL
+const logger = (req, res, next) => {
+  console.log(`inside the logger:  ${req.method} ${req.url}`);
+  next();
+};
+
+// JWT verification middleware
+const verifyJWT = (req, res, next) => {
+  const token = req?.cookies?.token;
+  console.log("cookie in the middleware", token);
+  if (!token) {
+    return res
+      .status(401)
+      .send({ error: true, message: "Unauthorized access" });
+  }
+
+  // Verify the token
+  jwt.verify(token, process.env.JWR_ACCESS_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(403).send({ error: true, message: "Forbidden access" });
+    }
+    req.decoded = decoded;
+    console.log("decoded token", decoded);
+  });
+  next();
+};
 
 // MongoDB client
 const uri = process.env.MONGO_URI;
@@ -38,6 +73,23 @@ async function run() {
       .db("careerCode")
       .collection("applications");
 
+    // jwt token related api
+    app.post("/jwt", async (req, res) => {
+      const { email } = req.body;
+      const user = { email };
+      const token = jwt.sign(user, process.env.JWR_ACCESS_SECRET, {
+        expiresIn: "1h",
+      });
+
+      // set token in cookie
+      res.cookie("token", token, {
+        httpOnly: true,
+        secure: false,
+      });
+
+      res.send({ success: true });
+    });
+
     // Jobs API
     app.get("/jobs", async (req, res) => {
       const email = req.query.email;
@@ -66,8 +118,16 @@ async function run() {
     });
 
     // job application related APIs
-    app.get("/applications", async (req, res) => {
+    app.get("/applications", logger, verifyJWT, async (req, res) => {
       const email = req.query.email;
+
+      // console.log("inside applications api", req.cookies);
+      if (email !== req.decoded.email) {
+        return res
+          .status(403)
+          .send({ error: true, message: "Forbidden access" });
+      }
+
       const query = {
         applicant: email,
       };
